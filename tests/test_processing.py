261f1f76099dbbeeb8158a6a11e533b58bda6d95
Zhi Lin Lu <zhilinlu12@gmail.com>
2025-04-28 18:47:48 -0400

    Breaking up the processing file into individual componenets for ease of testing. Separated into four steps, process_zip() for merging mbta zip file into single csv for cleaning. process_mbta() and process_weather() for cleaning and processing mbta and weather data. combine_data() to combine cleaned mbta and weather data for data file used in model creation. Modified test_processing with imports for functions, still need to implement the actual tests

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,1 +1,21 @@
 # Tests for processing
+
+# Uses a snippet of data for testing to see if processing.py works
+import pytest
+
+
+
+from src.data.processing import process_zip
+from src.data.processing import process_mbta
+from src.data.processing import process_weather
+from src.data.processing import combine_data
+
+def test_process_zip(mbta_data_zip):
+
+def test_process_mbta(mbta_data):
+  
+def test_process_weather(weather_data):
+
+def test_combine_data(celaned_mbta, cleaned_weather):
+  
+  
